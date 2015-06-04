786d29bc3f771dfccfb3384127df466c72f006b9
Zhenhua Ma <mazhenhua@huawei.com>
2015-06-04 13:22:50 +0800

    angler: init.angler.rc: copy mcfg config files
    - Copy config files
    - change the new config to correct ownership.
    - set flag to indicate copy is completed.
    
    Change-Id: Ic080efa92d8441d31ca6bbcbe22a69e792001f11

--- device.mk
+++ device.mk
@@ -39,6 +39,10 @@ PRODUCT_COPY_FILES += \
 
 
 PRODUCT_COPY_FILES += \
+    device/huawei/angler/init.mcfg.sh:system/etc/init.mcfg.sh
+
+# Media
+PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
