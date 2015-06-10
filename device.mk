e54f7e92c7077b621ab56a070087737d5b563cfe
Mekala Natarajan <mekalan@codeaurora.org>
2015-06-09 21:10:02 -0700

    device.mk: add media_profiles.xml for camcorder bringup
    Change-Id: I40eed46a2d6c12283168679d23aa16559e63fcef

--- device.mk
+++ device.mk
@@ -44,7 +44,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/huawei/angler/media_codecs.xml:system/etc/media_codecs.xml
+    device/huawei/angler/media_codecs.xml:system/etc/media_codecs.xml \
+    device/huawei/angler/media_profiles.xml:system/etc/media_profiles.xml
 
 # Audio
 PRODUCT_COPY_FILES += \
