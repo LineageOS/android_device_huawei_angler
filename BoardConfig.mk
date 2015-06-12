a444cfb1d3c29a3ab072709fe454af73a16ad4fa
Naveen Ramaraj <nramaraj@codeaurora.org>
2015-06-11 18:03:50 -0700

    angler: Enable Interaction Boost via powerhal
    Change-Id: Ied21cc77d1ddaa8a69c9f5f43fb5e6589c85f528

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,8 @@ TARGET_USES_64_BIT_BINDER := true
 
 TARGET_USES_AOSP := true
 
+TARGET_USES_INTERACTION_BOOST := true
+
 # Force camera module to be compiled only in 32-bit mode on 64-bit systems
 # Once camera module can run in the native mode of the system (either
 # 32-bit or 64-bit), the following line should be deleted
