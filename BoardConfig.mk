9e545a5842d12ddbd70f2c139e52d3e2c5970137
Tim Murray <timmurray@google.com>
2015-06-15 12:02:17 -0700

    Update angler CPU variants.
    bug 21663541
    
    Change-Id: Ib65b8b5054605680c06ee6abd85dcedc12896e56

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,19 +21,13 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-# TODO: FIX
-TARGET_2ND_CPU_VARIANT := cortex-a9
-#ifneq ($(TARGET_USES_AOSP), true)
-#TARGET_2ND_CPU_VARIANT := cortex-a53
-#else
-#TARGET_2ND_CPU_VARIANT := cortex-a9
-#endif
+TARGET_2ND_CPU_VARIANT := cortex-a7
 
 ENABLE_CPUSETS := true
 
