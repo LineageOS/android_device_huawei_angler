9cd052997ec357ca93aec07603716100a715800d
Ed Tam <etam@google.com>
2015-06-15 01:23:06 -0700

    Rename config partition to frp
    Change-Id: I10430154060e5ebd1a8f0c4935d0d10bcb8ff632

--- device.mk
+++ device.mk
@@ -242,7 +242,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.custom_ecc=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.frp.pst=/dev/block/platform/soc.0/f9824900.sdhci/by-name/config
+   ro.frp.pst=/dev/block/platform/soc.0/f9824900.sdhci/by-name/frp
 
 # For SPN display
 PRODUCT_COPY_FILES += \
