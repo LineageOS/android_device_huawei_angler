a16a66a4311a61704f9a5223b8b0b60448c313cd
Adnan Begovic <adnan@cyngn.com>
2015-11-02 14:46:22 -0800

    angler: squash of commits for lineage bring-up [1]
    In addition to the squashed commits:
    
    *) Add nfc_extras that was buried within a cm-13.0
       proprietary files commit.
    
    *) Prevent build error relating to writing hardware details
       in media files if TARGET_PRODUCT is not aosp_angler.
    
    Author: nuclearmistake <nuclearmistake@gmail.com>
    Date:   Wed Jul 20 17:26:24 2016 -0400
        angler: correct naming of blob makefile in aosp_angler.mk
        Change-Id: I2508b105a1e8b1c1218b59cbb8bd40476aa69ac3
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Tue Nov 24 10:45:23 2015 -0800
        Set TARGET_KEYMASTER_WAIT_FOR_QSEE := true
        See http://review.cyanogenmod.org/#/c/117168/
        for background.
        Change-Id: I1da0243aa7d5a99016e19875f799a883d57eb97e
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Tue Nov 17 16:41:33 2015 -0800
        Ensure cm apns are always used
        Change-Id: I39aaaf17061b82858bc513448f02ecbb54bc388d
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Mon Nov 16 23:31:08 2015 -0800
        angler: install vendor modules to system
        Change-Id: Iaa2137ad6ffb3772974588c1fa76ca88d89b6328
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Nov 6 16:59:22 2015 -0800
        Set BOARD_NEEDS_VENDORIMAGE_SYMLINK
        google stock has this and various /vendor blobs
        contain hardcoded /system/vendor/ paths where
        the content lives in /vendor.
        Change-Id: Idab6384d24857fa4ab3a50a6d13e85ac4ea4964e
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Tue Nov 3 16:03:19 2015 -0800
        angler: specify kernel and header arch
        Change-Id: I86a3d0f3e5982c9ca9ecadd07eb330e484733da1
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Mon Nov 2 15:32:47 2015 -0800
        angler: Remove vendor files restriction.
        Change-Id: Ice17b936d43f8003e809b4669536787f4f225837
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Mon Nov 2 14:46:59 2015 -0800
        angler: Create cm_angler target.
        Change-Id: Ie0c8079a9ed6baec9e54ed9752600dadbacb3593
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Mon Nov 2 14:46:44 2015 -0800
        angler: Support inline kernel compile.
        Change-Id: I580daa5ab6d7f2d0abaf824726574198c6f214cf
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Mon Nov 2 14:46:22 2015 -0800
        angler: Add cm.dependencies.
          Sync for in line kernel compile.
        Change-Id: I28de7fe83536bcfd38b7d0bf57e30b7e35179613
    
    Author: Abhisek Devkota <ciwrl@cyanogenmod.com>
    Date:   Mon Mar 14 09:54:29 2016 -0800
        Disable AnglerLayout [10]
        We use the ones in Trebuchet
        Change-Id: I200a112b799ff58a0ee245bf9e0f8adec2c8bf07
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Fri Oct 21 23:21:19 2016 -0500
        angler: comment out product package charger_res_images
        We inherit cm_charger_res_images via vendor/cm instead.
        Change-Id: Ic9f2f90e6f2213b36146f996af6d4fb258c2ab66
        (cherry picked from commit a18a78290c25d2179caa9c9106fac8db82c99bc6)
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Fri Nov 18 02:36:33 2016 -0700
        angler: Remove enhanced NFC feature
        This is no longer used anywhere.
        Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Sun Dec 25 06:00:22 2016 -0800
        angler: add missing line continuation slash
        This adds the perfd property that's missing in the build.prop
        Change-Id: I9342ef2c7add9c11bf7e7bd35c87e18f905f19a7
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sun Jan 1 19:59:02 2017 -0800
        angler: rebrand cyanogenmod -> lineageos
        And cm -> lineage
        Remove unused overlay-cm overlay dir config
        Change-Id: I590ab27212d8490cf9322880261ae7303d9f29ad
    
    Change-Id: I39aaaf17061b82858bc513448f02ecbb54bc388d

---
+++ lineage.mk
@@ -0,0 +1,21 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+
+TARGET_VENDOR := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:8.0.0/OPR6.170623.013/4283548:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 8.0.0 OPR6.170623.013 4283548 release-keys"
