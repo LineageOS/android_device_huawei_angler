1d91935a21e8f052ebe1e12d97ccc023a334812c
Adnan Begovic <adnan@cyngn.com>
2015-11-02 14:46:59 -0800

    angler: Create cm_angler target.
    Change-Id: Ie0c8079a9ed6baec9e54ed9752600dadbacb3593

---
+++ cm.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/moto/angler/overlay-cm
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := cm_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+
+TARGET_VENDOR := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:6.0/MDA89D/2294819:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0 MDA89D 2294819 release-keys"
