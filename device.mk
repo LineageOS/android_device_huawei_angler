a692d0c0fcb32368a9b5ba3354a75a5af956af34
beanstown106 <nbenis106@gmail.com>
2015-11-09 20:58:44 -0500

    angler: facelock properties
    Change-Id: I1be3c998b083da85a4109eca13d4f455c9e6f37a

--- device.mk
+++ device.mk
@@ -391,6 +391,15 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwu
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
+# facelock properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
+
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8994.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
 
