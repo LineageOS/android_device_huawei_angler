1132b8e5682362782b17730195a8a76f720f15c4
Sam Mortimer <sam@mortimer.me.uk>
2015-11-09 22:42:17 -0800

    [WIP] Update blobs to MDB08M
    *) update to use dhacker29 shamu based extract/setup scripts
    
    *) update fingerprint (required to keep verify on /vendor happy)
    
    Change-Id: I4df671bcfc6cc3d61e10132d2d800f31351dfa8a

--- extract-files.sh
+++ extract-files.sh
@@ -1,68 +1,66 @@
 #!/bin/bash
-#
-# Copyright (C) 2013-2015 The CyanogenMod Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
 
+export VENDOR=huawei
+export DEVICE_VENDOR=huawei
+export DEVICE=angler
+export PROPRIETARY_FILES=proprietary-blobs.txt
+
+# Check to see if the user passed a folder in to extract from rather than adb pull
+if [ $# -eq 1 ]; then
+    COPY_FROM=$1
+    test ! -d "$COPY_FROM" && echo error reading dir "$COPY_FROM" && exit 1
+fi
 
 set -e
 
-VENDOR=huawei
-DEVICE=angler
-
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-
-for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | sed -e 's#^/system/##g' | sed -e "s#^-/system/##g"`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-
-    if [ "$SRC" = "adb" ]; then
-        if ! adb shell ls /system/$FILE | grep "No such file" &>/dev/null; then
-            adb pull /system/$FILE $BASE/$FILE
-        else
-            echo "ERROR: Pull file /system/$FILE from a device running CyanogenMod"
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        echo "Extracting /system/$FILE ..."
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
         fi
-    else
-        if [ -e $SRC/$FILE ]; then
-            cp $SRC/$FILE $BASE/$FILE
-        else
-            echo "ERROR: Pull file /system/$FILE from a device running CyanogenMod"
+        DIR=`dirname $FILE`
+        if [ ! -d $2/$DIR ]; then
+            mkdir -p $2/$DIR
         fi
-    fi
+        if [ "$COPY_FROM" = "" ]; then
+            # Try destination target first
+            if [ -f /system/$DEST ]; then
+                adb pull /system/$DEST $2/$DEST
+            else
+                # if file does not exist try OEM target
+                if [ "$?" != "0" ]; then
+                    adb pull /system/$FILE $2/$DEST
+                fi
+            fi
+        else
+            # Try destination target first
+            if [ -f $COPY_FROM/$DEST ]; then
+                cp $COPY_FROM/$DEST $2/$DEST
+            else
+                # if file does not exist try OEM target
+                if [ "$?" != "0" ]; then
+                    cp $COPY_FROM/$FILE $2/$DEST
+                fi
+            fi
+        fi
+        # Fixup xml files
+        if [[ "$2/$DEST" =~ .xml$ ]]; then
+            xmlheader=$(grep '^<?xml version' "$2/$DEST")
+            grep -v '^<?xml version' "$2/$DEST" > "$2/$DEST".temp
+            (echo "$xmlheader"; cat "$2/$DEST".temp ) > "$2/$DEST"
+            rm "$2/$DEST".temp
+        fi
+    done
+}
 
-    # fixup xml files
-    if [[ "$BASE/$FILE" =~ .xml$ ]]; then
-        xmlheader=$(grep '^<?xml version' "$BASE/$FILE")
-	grep -v '^<?xml version' "$BASE/$FILE" > "$BASE/$FILE".temp
-	(echo "$xmlheader"; cat "$BASE/$FILE".temp ) > "$BASE/$FILE"
-	rm "$BASE/$FILE".temp
-    fi
-done
+DEVICE_BASE="../../../vendor/$VENDOR/$DEVICE/proprietary"
+rm -rf "$DEVICE_BASE"/*
+
+# Extract the device specific files
+extract "../../$DEVICE_VENDOR/$DEVICE/$PROPRIETARY_FILES" "$DEVICE_BASE"
 
 ./setup-makefiles.sh
