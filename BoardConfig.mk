875006f63de48feefed1044dfdc628c0ff4da296
Sam Mortimer <sam@mortimer.me.uk>
2015-11-16 23:46:39 -0800

    Enable cmhw support
    Change-Id: I3ca8c97e1949ec8341ac2cba2855d8d7733d5283

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,4 +146,9 @@ WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
+# CMHW
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS := \
+    hardware/cyanogen/cmhw
+
 -include vendor/huawei/angler/BoardConfigVendor.mk
