659e7680be51b9115d7977cba6417be3feb01d39
Chih-Hung Hsieh <chh@google.com>
2015-12-18 14:26:43 -0800

    Move angler to clang by default.
    Bug: 23163853
    Change-Id: I7dd08dde7467c7e8847f584e53b1bbb58fc81b3e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,4 +128,6 @@ WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
+USE_CLANG_PLATFORM_BUILD := true
+
 -include vendor/huawei/angler/BoardConfigVendor.mk
