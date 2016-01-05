bce997fa63ec8b6d6ee2a1ae8226d3c903ebb381
Sam Mortimer <sam@mortimer.me.uk>
2016-01-04 19:34:53 -0800

    Update build fingerprint to MMB29P
    Change-Id: I784ae5569201a518f1bed6962eb96feb3f7f04e2

--- cm.mk
+++ cm.mk
@@ -22,5 +22,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29P/2473553:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys"
