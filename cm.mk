2ee82b8cb7efe381b065b9c533286191edb360f8
nuclearmistake <nuclearmistake@gmail.com>
2016-01-06 07:33:10 -0500

    Angler: fix build id mismatch issues with build at MMB29T
    Change-Id: If8d84ede7881970e85fe3521611d674d17c0a585

--- cm.mk
+++ cm.mk
@@ -23,4 +23,5 @@ TARGET_VENDOR := huawei
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29P/2473553:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys"
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29P 2473553 release-keys" \
+    BUILD_ID=MMB29P
