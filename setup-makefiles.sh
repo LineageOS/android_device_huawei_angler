b0a39f4430fd5f7e2542a7399e2092a2d9496220
Sam Mortimer <sam@mortimer.me.uk>
2016-02-05 14:39:52 -0800

    Add HotwordEnrollment and enable BOARD_SUPPORTS_SOUND_TRIGGER
    Stands a chance now that Steve did:
    http://review.cyanogenmod.org/#/c/137818/
    
    Change-Id: Ifa6aa61592f9cf3bfe885d3f8b8a321c5839bf0a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -248,9 +248,7 @@ for PRIVAPK in `ls ../../../$OUTDIR/proprietary/priv-app/*/*apk`; do
   fi
     privapkname=`basename $PRIVAPK`
     privmodulename=`echo $privapkname|sed -e 's/\.apk$//gi'`
-  if [[ $privmodulename = BuaContactAdapter || $privmodulename = MotoSignatureApp ||
-      $privmodulename = TriggerEnroll || $privmodulename = TriggerTrainingService ||
-      $privmodulename = VZWAPNService ]]; then
+  if [[ $privmodulename = HotwordEnrollment ]]; then
     signature="PRESIGNED"
   else
     signature="platform"
