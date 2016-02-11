fbae5ff40c8f86dea3297d306ee1501a309cae9e
Joshndroid <fmxjoshiewah@gmail.com>
2016-02-11 17:11:19 +1000

    Snap : Build and import Snap Camera
    Thanks to Kevin Bayer for assisting fix for config.xml
    
    Change-Id: Ie31ad6a0beae0496861d9f32b7834b23c22ca369

--- device.mk
+++ device.mk
@@ -175,7 +175,8 @@ PRODUCT_PACKAGES += \
     libmmcamera_interface2 \
     libmmjpeg_interface \
     libqomx_core \
-    mm-qcamera-app
+    mm-qcamera-app \
+    Snap
 
 # Sensor & activity_recognition HAL
 PRODUCT_PACKAGES += \
