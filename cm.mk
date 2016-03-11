1ced5bd75c594422e59b89c44ad492447df8cf6c
Joshndroid <fmxjoshiewah@gmail.com>
2016-03-11 21:08:33 +1000

    Angler: update fingerprint to MHC19I
    Change-Id: I66150a1f22f1133e50de6e006e6fb6e6f0f4a104

--- cm.mk
+++ cm.mk
@@ -22,5 +22,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29V/2554798:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29V 2554798 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MHC19I/2590160:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19I 2590160 release-keys"
