28d0ea47e027f68dfdd09d1b7f533af13e75c82c
Roman Birg <roman@cyngn.com>
2016-03-22 07:44:05 -0700

    angler: squash of verity commits [5]
    In addition: disable use of fstab.aosp_angler
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Thu Mar 31 13:35:28 2016 -0700
        angler: only enable verity on user builds
        Change-Id: Id515efda13d8c88b97ff49d4d8d85024a84a8688
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Author: Roman Birg <roman@cyngn.com>
    Date:   Tue Mar 22 07:44:05 2016 -0700
        angler: don't verify /vendor
        dm-verity will not work properly as we don't compile in the vendor
        image alongside the system image, so the verity signing process will be
        partially always incomplete.
        We can still enable verity on /system.
        Change-Id: I7d6b8272ab3882dc15c1f730379f22c2fbbe7504
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Change-Id: Ifb60d527b8d30fb50e8de5f1fa3c71264155b812

--- device.mk
+++ device.mk
@@ -479,7 +479,13 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4358
 PRODUCT_COPY_FILES += \
     device/huawei/angler/location/etc/gps.conf:system/etc/gps.conf:qcom
 
-# setup dm-verity configs.
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
+# only include verity on user builds for CM
+ifeq ($(TARGET_BUILD_VARIANT),user)
+  PRODUCT_COPY_FILES += device/huawei/angler/fstab-verity.angler:root/fstab.angler
+
+  # setup dm-verity configs.
+  PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/system
+  # don't check verity on vendor partition as we don't compile it with the boot and system image
+  # PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
+  $(call inherit-product, build/target/product/verity.mk)
+endif
