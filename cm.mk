dbe81b61121f498fbd54eda21585c6e8ae9d13b2
nuclearmistake <nuclearmistake@gmail.com>
2016-05-02 20:39:42 -0400

    Update fingerprint to MTC19T
    Change-Id: Ia5a9d08859a011eb4ed747fa6eb848fda294b0d4

--- cm.mk
+++ cm.mk
@@ -22,5 +22,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MHC19Q/2705526:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MHC19Q 2705526 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC19T/2741993:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC19T 2741993 release-keys"
