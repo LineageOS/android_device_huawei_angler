c7dcb6b694736a059b295e77fc0a358b229d0aaa
Andrew Hsieh <andrewhsieh@google.com>
2016-07-20 23:41:58 -0700

    New target device angler_treble
    Device angler_treble differs from angler only slightly:
    both "opt-in" treble (ENABLE_TREBLE := true), but angler_treble
    has binderized HALs while angler wraps conventional HALs
    (BOARD_WRAPS_CONVENTIONAL_HALS := true)
    
    BUG=30160121
    
    Change-Id: I9e3baf48312d6ef26e137f80e3241aaca74b3491

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter angler, $(TARGET_DEVICE)),)
+ifneq ($(filter angler angler_treble, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
--- angler_treble/BoardConfig.mk
+++ angler_treble/BoardConfig.mk
@@ -1,4 +1,5 @@
-# Copyright 2015 The Android Open Source Project
+#
+# Copyright (C) 2015 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,27 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+include device/huawei/angler/BoardConfig.mk
 
-ifneq ($(filter angler angler_treble, $(TARGET_DEVICE)),)
-
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-	voice_processing_descriptors.c
-
-LOCAL_C_INCLUDES += \
-	$(call include-path-for, audio-effects)
-
-LOCAL_MULTILIB := $(AUDIOSERVER_MULTILIB)
-
-LOCAL_MODULE := libqcomvoiceprocessingdescriptors
-
-LOCAL_MODULE_RELATIVE_PATH := soundfx
-
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-endif
+ENABLE_TREBLE := true
+BOARD_WRAPS_CONVENTIONAL_HALS := false
