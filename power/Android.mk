839e0f58c3827fd3f7e8b1ce1409100c32377ba7
Sam Mortimer <sam@mortimer.me.uk>
2016-09-24 16:29:54 -0700

    angler: squash of enabling various features [4]
    In addition:
    
    *) Remove type sysfs_vibrator from sepolicy/file.te
       It's now defined in system/sepolicy.
    
    Author: dhacker29 <dhackerdvm@gmail.com>
    Date:   Sat Oct 31 00:26:49 2015 -0400
        powerHAL: Adds functionality to enable/disable double-tap wake gesture.
        Adds support for set_feature() function to enable or disable the features.
        Double-tap wake gesture is the first feature that has been added which
        uses this framework.
        Set overlay to have double tap to wake support
        Change-Id: I60860c92bf383fdba5d8f4e1a99a809a5f89a315
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Nov 12 19:18:25 2015 -0800
        Enable doze mode [6]
        Change-Id: I45f848dc73093c430500026e789ec0e295b5edaf
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Mon Nov 16 23:46:39 2015 -0800
        Enable cmhw support
        Change-Id: I3ca8c97e1949ec8341ac2cba2855d8d7733d5283
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Nov 12 19:54:18 2015 -0800
        Define which hardware keys are present
        Change-Id: I9165dc2056e1f8a9ab194aa4b12f49cfe2b7c882
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Nov 6 17:19:12 2015 -0800
        Enable multicolor LED
        Change-Id: I6efb70d98d58d76e98a28659dc9cc01c26ac90ef
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Wed Nov 18 23:24:54 2015 -0800
        Inform vendor/cm that BFQ is in use
        Change-Id: Ie68437ad72babb775f09a61515f3c076523986ea
    
    Author: doc <doc.divxm@gmail.com>
    Date:   Sun Feb 15 21:44:03 2015 +0200
        angler: Enable Wifi Display and fix screen mirroring
        Change-Id: I9f6a58c93b11aeb014736bfd99be2766c06c132e
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Nov 14 21:49:41 2015 -0800
        Add cmhw vibrator intensity control
        Change-Id: I942bd1aa18f43b00d9773428191016fab3a75036
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Sep 24 16:29:54 2016 -0700
        Fixup use of FileUtils in VibratorHW.java
        Change-Id: Iff6697691afea1f3032ae2da20e73206beefc548
    
    Author: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Date:   Mon Nov 21 17:01:56 2016 +0800
        angler: Switch to the unified LED capabilities overlay
        The old overlays have now been removed in favour
        of a single unified and extensible overlay.
        Change-Id: I56f5bfa2baa7bb9bf546bd5e59cebb74ac71732f
    
    Author: AdrianDC <radian.dc@gmail.com>
    Date:   Sun Aug 23 01:16:17 2015 +0200
        angler: liblights: LED brightness LibLights control
         * Add the brightness features to the LibLights.
         * Enable LIGHTS_ADJUSTABLE_NOTIFICATION_LED_BRIGHTNESS,
            which replaces the older cmsdk overlay
            config_adjustableNotificationLedBrightness
        Change-Id: Ib21879b2b61afffc194294adbeb3818b12406374
        Signed-off-by: AdrianDC <radian.dc@gmail.com>
    
    Change-Id: Iaa54d67c9e6e16ad263ca84ecf89a27802a2baad

--- power/Android.mk
+++ power/Android.mk
@@ -7,7 +7,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MULTILIB := both
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c feature.c
 
 # Include target-specific files.
 
