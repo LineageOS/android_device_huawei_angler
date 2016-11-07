fa7dc12b85deb05fa441dc22f396a9d77e93428d
Sam Mortimer <sam@mortimer.me.uk>
2016-11-07 11:46:43 -0800

    angler: update fingerprint to NBD91K
    Change-Id: I5aaf94af9b0511cfe7f22c9358d86de45a3ac3cf

--- cm.mk
+++ cm.mk
@@ -22,5 +22,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.0/NBD90X/3254009:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.0 NBD90X 3254009 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.0/NBD91K/3318877:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.0 NBD91K 3318877 release-keys"
