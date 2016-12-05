e55838668b03cc1e199940c1661b2c7bc8b6acb4
Sam Mortimer <sam@mortimer.me.uk>
2016-12-05 12:30:12 -0800

    angler: update fingerprint to NMF26F (7.1.1)
    Change-Id: I23f939bb9fd99781eefab5b253d654677bacfc9b

--- cm.mk
+++ cm.mk
@@ -19,5 +19,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:7.0/NBD91K/3318877:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 7.0 NBD91K 3318877 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:7.1.1/NMF26F/3425388:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.1 NMF26F 3425388 release-keys"
