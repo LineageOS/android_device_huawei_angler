02734ffb76086106f5b0ad813404aa657695dbc1
razorloves <razorloves@gmail.com>
2016-12-25 06:00:22 -0800

    angler: add missing line continuation slash
    This adds the perfd property that's missing in the build.prop
    
    Change-Id: I9342ef2c7add9c11bf7e7bd35c87e18f905f19a7

--- device.mk
+++ device.mk
@@ -327,7 +327,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # for perfd
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min_freq_0=384000
+    ro.min_freq_0=384000 \
     ro.min_freq_4=384000
 
 PRODUCT_PROPERTY_OVERRIDES += \
