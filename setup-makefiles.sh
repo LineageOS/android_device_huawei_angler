df23cbbcce399c1fb5861b071d7b3c923b277fc2
Sam Mortimer <sam@mortimer.me.uk>
2017-11-03 01:20:00 +0000

    angler: Build a vendor image
    Change-Id: Ieb91862c0727be82205585d464af1da9748ed1d7

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -52,5 +52,7 @@ write_headers
 # The standard blobs
 write_makefiles "$MY_DIR"/lineage-proprietary-blobs.txt
 
+write_makefiles "$MY_DIR"/lineage-proprietary-blobs-vendorimg.txt true
+
 # We are done!
 write_footers
