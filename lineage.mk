81e2bc91b43751fb9b5beed1ee0bc0ed859c5778
razorloves <razorloves@gmail.com>
2017-11-10 11:41:29 +0300

    angler: update fingerprint to OPM3.171019.013
    Change-Id: I5d7faaedbbea7f5821790d831a983e7ea7c855e4

--- lineage.mk
+++ lineage.mk
@@ -17,5 +17,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:8.0.0/OPR6.170623.013/4283548:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 8.0.0 OPR6.170623.013 4283548 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:8.1.0/OPM3.171019.013/4499252:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.013 4499252 release-keys"
