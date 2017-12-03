3389ddfdb6a57e7e656d883db32d78e5ca000e2a
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-12-03 15:49:30 +0100

    angler: fix perfd denial
    *[   99.959999] type=1400 audit(1512312240.377:8): avc: denied { signull } for pid=594 comm="perfd" scontext=u:r:perfd:s0 tcontext=u:r:system_server:s0 tclass=process permissive=1
    
    Change-Id: I558f2dbe9dd466b8f56ffc935b051c068a8bda7e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=angler androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 no_console_suspend
+BOARD_KERNEL_CMDLINE := androidboot.hardware=angler androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 no_console_suspend androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 BOARD_USES_ALSA_AUDIO := true
