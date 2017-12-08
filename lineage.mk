c6578c2394ccc82d5cdab9e20015e264fdddb3ac
Sam Mortimer <sam@mortimer.me.uk>
2017-12-07 16:42:10 -0800

    angler: update fingerprint to OPM1.171019.011
    Change-Id: I5d7faaedbbea7f5821790d831a983e7ea7c855e4

--- lineage.mk
+++ lineage.mk
@@ -17,5 +17,5 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:8.0.0/OPR5.170623.011/4397986:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 8.0.0 OPR5.170623.011 4397986 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:8.1.0/OPM1.171019.011/4448085:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM1.171019.011 4448085 release-keys"
