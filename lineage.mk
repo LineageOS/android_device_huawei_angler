bc036e17744f3cd719d0a31ea9334d474b4fb754
razorloves <razorloves@gmail.com>
2018-07-02 13:14:44 -0500

    angler: update build fingerprint to July 2018 OPM6.171019.030.E1
    Change-Id: Id4cc6f1fa02b1f190323249c97ee6c3aace5b69d

--- lineage.mk
+++ lineage.mk
@@ -17,6 +17,6 @@ TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM6.171019.030.B1 4768815 release-keys"
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM6.171019.030.E1 4805388 release-keys"
 
-BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM6.171019.030.B1/4768815:user/release-keys
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM6.171019.030.E1/4805388:user/release-keys
