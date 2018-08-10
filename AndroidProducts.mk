6c9478ee4dbd416f058add02416a561f9296a1c3
Bruno Martins <bgcngm@gmail.com>
2018-08-10 14:26:00 +0100

    angler: Consider lineage_angler product via the AOSP way
    Change-Id: Ib81b59da18b9efbec006b4aeba166d9ad7627b7e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2015 The Android Open Source Project
+#           2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +16,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_angler.mk
+    $(LOCAL_DIR)/aosp_angler.mk \
+    $(LOCAL_DIR)/lineage_angler.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_angler-userdebug
