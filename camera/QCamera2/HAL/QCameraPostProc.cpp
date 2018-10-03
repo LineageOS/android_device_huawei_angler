2d436c7f529804454db921cef8b968ff430af62b
razorloves <razorloves@gmail.com>
2018-10-03 17:38:49 -0500

    Revert "angler: QCamera2 printf format fixes [16]"
    * No longer needed. Proper fix applied to kernel
    
    This reverts commit e0f24dc0f5d301d9f91642ee907c6e3e248b7d3a.
    
    Change-Id: I583857aaacb210893a13eb81e451b96ed8a2dd79

--- camera/QCamera2/HAL/QCameraPostProc.cpp
+++ camera/QCamera2/HAL/QCameraPostProc.cpp
@@ -2363,12 +2363,12 @@ void *QCameraPostProcessor::dataSaveRoutine(void *data)
                         ssize_t written_len = write(file_fd, job_data->out_data.buf_vaddr,
                                 job_data->out_data.buf_filled_len);
                         if ((ssize_t)job_data->out_data.buf_filled_len != written_len) {
-                            ALOGE("%s: Failed save complete data %ld bytes "
+                            ALOGE("%s: Failed save complete data %d bytes "
                                   "written instead of %d bytes!",
                                   __func__, written_len,
                                   job_data->out_data.buf_filled_len);
                         } else {
-                            CDBG_HIGH("%s: written number of bytes %ld\n",
+                            CDBG_HIGH("%s: written number of bytes %d\n",
                                 __func__, written_len);
                         }
 
