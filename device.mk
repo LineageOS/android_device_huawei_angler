9bc89871718e4f58c8f4fd636c79cf2283c7c77b
razorloves <razorloves@gmail.com>
2018-10-03 17:32:45 -0500

    Revert "angler: don't verify /vendor"
    * No longer needed since we're building vendor now
    
    Partial revert of 2fb4b8346f15dc12821befacc91c239e5631f106.
    
    Change-Id: I7ded64fadb81c4293142bec1ac0ecf540b145313

--- device.mk
+++ device.mk
@@ -579,8 +579,7 @@ ifeq ($(TARGET_BUILD_VARIANT),user)
 
   # setup dm-verity configs.
   PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/system
-  # don't check verity on vendor partition as we don't compile it with the boot and system image
-  # PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
+  PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc.0/f9824900.sdhci/by-name/vendor
   $(call inherit-product, build/target/product/verity.mk)
 endif
 
