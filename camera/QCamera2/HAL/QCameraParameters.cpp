d7fad94a006077e525e5f2089767efff902ed31e
Giuseppe Maggio <jertlokk@gmail.com>
2019-01-01 17:12:54 +0000

    angler: QCamera: remove unused private fields
    device/huawei/angler/camera/QCamera2/HAL3/QCamera3Mem.h:123:10: error:
    private field 'mQueueAll' is not used [-Werror,-Wunused-private-field]
        bool mQueueAll;
             ^
    
    device/huawei/angler/camera/QCamera2/HAL/QCameraParameters.h:87:24:
    error: private field 'mParent' is not used
    [-Werror,-Wunused-private-field]
        QCameraParameters *mParent;
                           ^
    device/huawei/angler/camera/QCamera2/HAL/QCameraParameters.h:90:10:
    error: private field 'mScaleDirection' is not used
    [-Werror,-Wunused-private-field]
        bool mScaleDirection;   // 0: Upscaling; 1: Downscaling
    
    Change-Id: I4834c79effdc6d17b0ffbea1b68f5ac73f582dc9

--- camera/QCamera2/HAL/QCameraParameters.cpp
+++ camera/QCamera2/HAL/QCameraParameters.cpp
@@ -762,7 +762,7 @@ const QCameraParameters::QCameraMap<int>
  *==========================================================================*/
 QCameraParameters::QCameraParameters()
     : CameraParameters(),
-      m_reprocScaleParam(this),
+      m_reprocScaleParam(),
       m_pCapability(NULL),
       m_pCamOpsTbl(NULL),
       m_pParamHeap(NULL),
@@ -871,7 +871,7 @@ QCameraParameters::QCameraParameters()
  *==========================================================================*/
 QCameraParameters::QCameraParameters(const String8 &params)
     : CameraParameters(params),
-    m_reprocScaleParam(this),
+    m_reprocScaleParam(),
     m_pCapability(NULL),
     m_pCamOpsTbl(NULL),
     m_pParamHeap(NULL),
@@ -10607,11 +10607,9 @@ int32_t QCameraParameters::commitParamChanges()
  *
  * RETURN     : none
  *==========================================================================*/
-QCameraReprocScaleParam::QCameraReprocScaleParam(QCameraParameters *parent)
-  : mParent(parent),
-    mScaleEnabled(false),
+QCameraReprocScaleParam::QCameraReprocScaleParam()
+  : mScaleEnabled(false),
     mIsUnderScaling(false),
-    mScaleDirection(0),
     mNeedScaleCnt(0),
     mSensorSizeTblCnt(0),
     mSensorSizeTbl(NULL),
--- camera/QCamera2/HAL3/QCamera3Mem.h
+++ camera/QCamera2/HAL3/QCamera3Mem.h
@@ -120,7 +120,6 @@ private:
     int allocOneBuffer(struct QCamera3MemInfo &memInfo,
             unsigned int heap_id, size_t size);
     void deallocOneBuffer(struct QCamera3MemInfo &memInfo);
-    bool mQueueAll;
     uint32_t mMaxCnt;
 };
 
