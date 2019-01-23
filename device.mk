09d0d90d77012ed52febc1c89fc216f43866610b
razorloves <razorloves@gmail.com>
2019-01-23 01:47:45 -0600

    angler: Enable p2p for wi-fi direct
    Override inherited configs to enable p2p.
    Configs copied from hardware/broadcom/wlan and removed 'p2p_disabled=1'.
    
    Change-Id: I4c80a6d2e599913f48d3a21fa62125bcc907e39a

--- device.mk
+++ device.mk
@@ -560,6 +560,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8994.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
 
+# WiFi configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # copy wlan firmware
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4358/device-bcm.mk)
 
