00570254ae4d9802ccf3e6ab847229efb51f899e
Anay Wadhera <anay1018@gmail.com>
2019-09-13 00:01:51 -0700

    angler: update config.fs for precreated root mountpoints
    Change-Id: I5aa1b96f1ac3fb00d647c1554e47ca21b5ca1290

--- config.fs
+++ config.fs
@@ -8,3 +8,13 @@ mode: 0700
 user: AID_CAMERA
 group: AID_SHELL
 caps: SYS_NICE
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
