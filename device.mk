b6d38ff6a31b5ec176dec81b1889e073314bde32
elektroschmock <elektroschmock78@googlemail.com>
2019-10-05 11:58:46 +0200

    angler: deprecate health@1.0
    For reference see:
    https://source.android.com/devices/tech/health/deprecation
    
    Change-Id: I2e47ad5266383e2d70ebb24e0226847971fad93e

--- device.mk
+++ device.mk
@@ -348,8 +348,7 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-service.angler
 
 #USB HAL
 PRODUCT_PACKAGES += \
