8aa35ce4e2c2a90ab62f03173be24ee569a51480
Davide Garberi <dade.garberi@gmail.com>
2019-11-17 17:50:05 -0500

    Update thermal HAL to v2.0 mock implementation.
    Change-Id: I6696db69b3aa34012ebff41d1304bffab7f5b00c

--- device.mk
+++ device.mk
@@ -342,9 +342,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
 
 # Thermal HAL
-#PRODUCT_PACKAGES += \
-#    thermal.angler \
-#    android.hardware.thermal@1.0-impl
+PRODUCT_PACKAGES += \
+    thermal.angler \
+    android.hardware.thermal@2.0-service.mock
 
 #GNSS HAL
 PRODUCT_PACKAGES += \
