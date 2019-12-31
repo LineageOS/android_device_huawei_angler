facd1a8814f1d24d89abe7aaf7d47cb8a93d9025
Anay Wadhera <awadhera@berkeley.edu>
2019-12-31 10:20:51 -0800

    angler: build camera provider and create legacy instance
    * taken from op2
    
    Change-Id: I86346d40bbabe7a63d2900d3c5de56d1f01705d4

--- device.mk
+++ device.mk
@@ -247,6 +247,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.msm8994 \
     libcamera \
     libmmcamera_interface \
