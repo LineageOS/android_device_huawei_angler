a68beb503b16f3303c25295e83df662f40f65ca3
Anay Wadhera <awadhera@berkeley.edu>
2020-02-09 11:09:06 -0800

    angler: build qti telephony packages
    Change-Id: I5922a860b3174a192806a044f48e7029cacce2d1

--- device.mk
+++ device.mk
@@ -433,6 +433,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_cdma_sub=0
 
+PRODUCT_PACKAGES += \
+    telephony-ext \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # LTE, CDMA, GSM/WCDMA
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=10 \
