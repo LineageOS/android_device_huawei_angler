f08ea8a933a461eaf06793eaa8df92c9381bfec2
Jiyong Park <jiyong@google.com>
2020-03-11 21:50:44 -0500

    TARGET_FLATTEN_APEX=true for Nexus 6P
    APEXes are flattened for Nexus 6P. Kernel is too old to support APEXes
    without performance regression.
    
    Bug: 123684826
    Change-Id: Idf54b059be056b353caae3fec276055f6a78eab7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,6 +185,8 @@ TARGET_FS_CONFIG_GEN += device/huawei/angler/config.fs
 DEVICE_MANIFEST_FILE := device/huawei/angler/manifest.xml
 DEVICE_MATRIX_FILE := device/huawei/angler/compatibility_matrix.xml
 
+TARGET_FLATTEN_APEX := true
+
 -include vendor/huawei/angler/BoardConfigVendor.mk
 
 # Legacy blob support
