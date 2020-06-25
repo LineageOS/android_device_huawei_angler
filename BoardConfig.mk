c90ba93e1e5ae19e9467e51c3a7a32bd3aef522e
Swetha Chikkaboraiah <schikk@codeaurora.org>
2020-06-25 14:19:22 +0200

    angler: Make swiotlb 4MB from command line parameter.
    This parameter "swiotlb=2048" sets the size of software
    IO TLB to defualt value, which translates to 4MB.
    The default size is 64MB. This will help in reducing
    kernel static by ~60MB.
    
    Change-Id: Id74ad0d92a5190720029ceb44585adf5ea579622

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
-BOARD_KERNEL_CMDLINE := androidboot.hardware=angler androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 no_console_suspend
+BOARD_KERNEL_CMDLINE := androidboot.hardware=angler androidboot.console=ttyHSL0 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-3 no_console_suspend swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 # system-as-root
