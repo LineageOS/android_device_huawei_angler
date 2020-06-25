88dd44985d4197a07bcec92be9b71060ffa1274a
Bruno Martins <bgcngm@gmail.com>
2020-06-25 18:07:28 +0200

    angler: convert audio_effects.conf to XML
     * Based on https://android.googlesource.com/device/google/taimen/+/ecf82951a431324bd129b31495043dcfa00cfb1f
    
     * Additionally to the same tests done by Google, it was verified that
       with this change "XML effect configuration loaded successfully.",
       while before it showed: "XML effect configuration failed to load.".
    
    Change-Id: I42112845def8a4a88022a0319c46ffb178d30ff2

--- device.mk
+++ device.mk
@@ -75,7 +75,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     device/huawei/angler/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
-    device/huawei/angler/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    device/huawei/angler/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     device/huawei/angler/mixer_paths.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/mixer_paths.xml \
     device/huawei/angler/audio_platform_info_i2s.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_platform_info_i2s.xml \
     device/huawei/angler/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sound_trigger_mixer_paths.xml \
