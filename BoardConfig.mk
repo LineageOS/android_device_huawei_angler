08904fd11e9c0733be255789e72a9b747b62bc41
anayw2001 <awadhera@berkeley.edu>
2020-06-26 16:33:12 +0200

    angler: use aarch64-linux-android- as a toolchain prefix
    Change-Id: I0a5754d46a8fdbe3b8b656b86925ff0699c0f5e6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,10 +35,10 @@ TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := lineageos_angler_defconfig
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
